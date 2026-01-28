a2eacfae03ad629e0116d5d11f52b37bb9a662f4
Mario Gomes Ramos Loureiro <marioloureiro2@gmail.com>
2026-01-28 16:51:45 -0300

    a

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,6 +5,9 @@ async function status(request, response) {
   
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+ 
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = 
 
   response.status(200).json({
     updated_at: updatedAt,
