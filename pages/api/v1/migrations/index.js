04de6e5c003a66b1180e63838456caeef8008fc1
mario gomes <marioloureiro2@gmail.com>
2026-02-04 10:28:32 -0300

    # This is a combination of 2 commits. # This is the 1st commit message:
    fix /migrations endpoint connection bug
    
    # This is the commit message #2:
    
    fix /migrations endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,7 +3,17 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "$[request.method]" not allow`,
+    });
+  }
+
+  let dbClient;
+
+  try {
+  dbClient = await database.getNewClient();
 
   const defaultMigrationOptions = {
     dbClient: dbClient,
@@ -17,7 +27,6 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationsRunner(defaultMigrationOptions);
-    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
 
@@ -27,15 +36,16 @@ export default async function migrations(request, response) {
       dryRun: false,
     });
 
-    await dbClient.end();
-
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     };
 
     return response.status(200).json(migratedMigrations);
   }
-
-  return response.status(405).end();
-
-};
+} catch (error) {
+  console.error(error)
+  throw error;
+} finally {
+  await dbClient.end();
+}
+}
